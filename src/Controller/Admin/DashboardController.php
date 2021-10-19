657b702b442296bcf9f2b9365e231c6b72f1d50c
slydar <tdjennane@gmail.com>
2021-10-19 16:15:18 +0200

    admin crud

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\Lieu;
 use App\Entity\Sortie;
+use App\Entity\User;
+use App\Entity\Ville;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -12,18 +14,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
-    /**
-     * @Route("/admin", name="admin")
-     */
-    public function index(): Response
-    {
-        return parent::index();
-    }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('SortirCom');
+            ->setTitle('SortirCom Administration');
     }
 
     public function configureMenuItems(): iterable
@@ -31,5 +26,7 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Sorties', 'fas fa-list', Sortie::class);
         yield MenuItem::linkToCrud('Lieux', 'fas fa-list', Lieu::class);
+        yield MenuItem::linkToCrud('Villes', 'fas fa-list', Ville::class);
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-list', User::class);
     }
 }
--- src/Controller/Admin/SortieCrudController.php
+++ src/Controller/Admin/SortieCrudController.php
@@ -12,14 +12,5 @@ class SortieCrudController extends AbstractCrudController
         return Sortie::class;
     }
 
-    /*
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
-    }
-    */
+
 }

99265180b1d4f52b71537c1c59daf7dc69403c0c
slydar <tdjennane@gmail.com>
2021-10-20 17:55:24 +0200

    trip admin form

--- src/Controller/Admin/SortieCrudController.php
+++ src/Controller/Admin/SortieCrudController.php
@@ -4,6 +4,13 @@ namespace App\Controller\Admin;
 
 use App\Entity\Trip;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class SortieCrudController extends AbstractCrudController
 {
@@ -12,5 +19,20 @@ class SortieCrudController extends AbstractCrudController
         return Trip::class;
     }
 
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('tripName')->setLabel('nom'),
+            TextEditorField::new('description'),
+            DateTimeField::new('tripStartDate')->setLabel('Date de début'),
+            DateField::new('endDate')->setLabel('date de fin'),
+            IntegerField::new('duration')->setLabel('durée'),
+            DateField::new('deadlineRegistrationDate')->setLabel('date de fin des inscriptions'),
+            IntegerField::new('capacity')->setLabel('Capacité'),
+            AssociationField::new('place')->autocomplete()->setLabel('Nom du lieu de la sortie'),
+            AssociationField::new('organizer')->autocomplete()->setLabel('Organisateur'),
+
+        ];
+    }
 
 }

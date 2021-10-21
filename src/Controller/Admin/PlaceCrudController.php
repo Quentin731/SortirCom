78b4b7c84328454e4532d405843f05c846293eee
Quentin <quentin.marchandise17@gmail.com>
2021-10-21 10:10:27 +0200

    Add show_trips-view

--- src/Controller/Admin/PlaceCrudController.php
+++ src/Controller/Admin/PlaceCrudController.php
@@ -12,14 +12,13 @@ class PlaceCrudController extends AbstractCrudController
         return Place::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
             TextField::new('title'),
             TextEditorField::new('description'),
         ];
     }
-    */
+
 }

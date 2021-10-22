815681453a27161a309ab1f5034db81e135fc19a
Quentin <quentin.marchandise17@gmail.com>
2021-10-22 16:26:43 +0200

    Add cancel on trip

--- src/Controller/TripController.php
+++ src/Controller/TripController.php
@@ -1,50 +1,51 @@
 <?php
 
-namespace App\Controller;
+    namespace App\Controller;
 
-use App\Entity\Trip;
-use App\Form\CreateSortieType;
-use DateInterval;
-use DateTime;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+    use App\Entity\City;
+    use App\Entity\Trip;
+    use App\Form\CreateSortieType;
+    use App\Form\CancelationTripType;
+    use App\Repository\PlaceRepository;
+    use Doctrine\ORM\EntityManagerInterface;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
 
-class TripController extends AbstractController
-{
-
-    private $entityManager;
-
-    /**
-     * ProductController constructor.
-     * @param $entityManager
-     */
-    public function __construct(EntityManagerInterface $entityManager)
+    class TripController extends AbstractController
     {
-        $this->entityManager = $entityManager;
-    }
 
-    /**
-     * @Route("/trip/create", name="create_sortie")
-     */
-    public function index(Request $request): Response
-    {
-        $sortie = new Trip();
-        $form = $this->createForm(CreateSortieType::class, $sortie);
-        $form->handleRequest($request);
+        private $entityManager;
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($sortie);
-            $entityManager->flush();
+        /**
+         * ProductController constructor.
+         * @param $entityManager
+         */
+        public function __construct(EntityManagerInterface $entityManager)
+        {
+            $this->entityManager = $entityManager;
         }
 
-        return $this->render('sortie/create-index.html.twig', [
-            'createSortie' => $form->createView(),
-        ]);
-    }
+        /**
+         * @Route("/create_sortie", name="create_sortie")
+         */
+        public function index(Request $request): Response
+        {
+            $sortie = new Trip();
+            $form = $this->createForm(CreateSortieType::class, $sortie);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($sortie);
+                $entityManager->flush();
+            }
+
+            return $this->render('sortie/create-index.html.twig', [
+                'createSortie' => $form->createView(),
+            ]);
+        }
 
     /**
      * @Route("/trip/{id}", name="show_sortie")
@@ -102,11 +103,35 @@ class TripController extends AbstractController
         return $this->redirectToRoute('home', array("error" => $error));
     }
 
-    /**
-     * @Route("placeList/{id}", name="placeList")
-     */
-    public function getJsonPlaceList(City $city, PlaceRepository $placeRepository)
-    {
-        return $this->json($placeRepository->findBy(["city" => $city]));
+        /**
+         * @Route("placeList/{id}", name="placeList")
+         */
+        public function getJsonPlaceList(City $city, PlaceRepository $placeRepository)
+        {
+            return $this->json($placeRepository->findBy(["city" => $city]));
+        }
+
+        /**
+         * @Route("/trip/{id}/cancelation", name="cancelation")
+         */
+        public function cancelationTrip(Request $request, $id) : Response
+        {
+            $trip = $this->entityManager->getRepository(Trip::class)->find($id);
+            $form = $this->createForm(CancelationTripType::class, $trip);
+            $form->handleRequest($request);
+
+            $dateNow = date("Y-m-d H:i:s");
+
+            if ($form->isSubmitted() && $form->isValid() && $trip->getTripStartDate() > $dateNow) {
+                $trip->setState(1);
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($trip);
+                $entityManager->flush();
+                return $this->redirectToRoute('home');
+            }
+            return $this->render('sortie/cancel-index.html.twig', [
+                'form' => $form->createView(),
+                'trip' => $trip
+            ]);
+        }
     }
-}
---
+++ src/Form/CancelationTripType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Trip;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CancelationTripType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('cencelationReason', TextType::class, ['label' => 'Motif d\'annulation:'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Trip::class,
+        ]);
+    }
+}
